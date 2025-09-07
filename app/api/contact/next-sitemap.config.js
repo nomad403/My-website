584f568371b841ed5c2c717d9856e5833d2e6d58
Ricardo <bglennrichard.pro@yahoo.com>
2025-09-07 12:45:00 +0200

    File sitemap

--- app/api/contact/next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://nomad403.com', // ton domaine
-    generateRobotsTxt: true,         // crée aussi robots.txt
-    sitemapSize: 5000,               // par défaut, bon pour toi
-  }
-  
---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://nomad403.com',
+    generateRobotsTxt: true,        
+    sitemapSize: 5000,              
+  }
+  
